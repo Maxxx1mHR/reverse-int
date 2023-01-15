04392304228de31d0d874dfcf78614241ceb1497
Your Name <you@example.com>
2023-01-15 16:29:21 +0300

    feat: add reverse-int with use toString and reverse with for and get back with toNumber

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function reverse (n) {
-  
+    let string = n.toString();
+    let rez = '';
+    for (let i = 0; i < string.length; i++) {
+        rez = string[i] + rez;
+        if (rez == '-') {
+            rez = rez.substring (1);
+        }
+    }
+    return +rez;
 }
+
+
+// let r = -123;
+// const string = r.toString();
+
+// console.log(typeof(string));
+
+// let rez = '';
+// for (let i = 0; i < string.length; i++) {
+//     rez = string[i] + rez;
+//     if (rez == '-') {
+//         rez = rez.substring (1);
+//     }
+// }
+// console.log(rez);
+
